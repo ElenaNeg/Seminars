08e21e8244cf29e45b1f68c1b90183690209f43e
Elena Negerish <elenkaNeg@yandex.ru>
2022-08-09 20:43:09 +0300

    task1

---
+++ S_01/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+int num = 5;
+Console.WriteLine("My number is " + num);
+*/
+/*
+int num;
+
+Console.Write("Input a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Your number is " + num);
+*/
+// Задача 1. На вход получить число, и найти его квадрат 
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int quad = num * num;
+
+Console.Write("The quadrate of " + num + " is " + quad);
+Console.Write($"The quadrate of {num} is {quad}");
