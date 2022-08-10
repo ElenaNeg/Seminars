f28ce1fcef99838b75aac685c2841e12caa2fd2e
Elena Negerish <elenkaNeg@yandex.ru>
2022-08-10 22:32:51 +0300

    task with array

--- S_02/Program.cs
+++ S_02/Program.cs
@@ -54,6 +54,71 @@ int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(max);
 */
 
-// Попробуем избавится от множества переменных использую для этого массив.
+// Задача 2. Попробуем избавится от множества переменных использую для этого массив.
+/*
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//            0  1  2   3  4  5  6  7  8
+int[] array = {11,211,31,41,15,61,17,18,19};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+*/
+
+//Задача 3 для любого колличеста элементов массива.
+/*
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1; 
+    index++;
+}
+*/
+//Запишем 2 вариант полее лучше.
+/*
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1; 
+    index++;
+}
+*/
+//Создаем задачку, чтобы массив сам определял свою длину. 
+void FillArray(int[] collection);
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
 
 
+//int[] array = new int[10];
